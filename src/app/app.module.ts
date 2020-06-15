74c0c31775cd316e59711c82d0545560f7dc1ca3
gglymph <gglymph@gmail.com>
2020-06-15 16:40:01 -0400

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TextboxComponent } from './controls/textbox/textbox.component';
 import { RadioButtonsComponent } from './controls/radio-buttons/radio-buttons.component';
+import { SelectComponent } from './controls/select/select.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HelloComponent, TextboxComponent, RadioButtonsComponent ],
+  declarations: [ AppComponent, HelloComponent, TextboxComponent, RadioButtonsComponent, SelectComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
