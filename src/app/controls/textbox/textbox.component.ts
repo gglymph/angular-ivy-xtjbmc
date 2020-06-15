c551403ce1fbe6dc7617fee6dbeccd90c2a6bc46
gglymph <gglymph@gmail.com>
2020-06-15 14:08:47 -0400

    1 file modified, 3 added

---
+++ src/app/controls/textbox/textbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'publiq-textbox',
+  templateUrl: './textbox.component.html',
+  styleUrls: ['./textbox.component.css']
+})
+export class TextboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
