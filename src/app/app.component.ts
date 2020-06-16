10776968290e3f380c4dabd8d92361b3662fd927
gglymph <gglymph@gmail.com>
2020-06-16 14:34:52 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component, VERSION } from "@angular/core";
-import { FormBuilder, Validators } from "@angular/forms";
+import { Component, VERSION, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   formGroupName = this.fb.group({
     textBox: [null, Validators.required],
-    select:  [null, Validators.required]
+    select:  [null, Validators.required],
+    array: this.fb.array([])
   });
+
+  get arr() {
+    return this.formGroupName.get('array') as FormArray;
+  }
+
+  ngOnInit() {
+    for (let i = 0; i < 5; i++) {
+      const formGroup = this.fb.group({
+        cntrl: `Control ${i}`
+      })
+    }
+  }
 
   constructor(private fb: FormBuilder) {}
 
