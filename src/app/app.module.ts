b5720f9d420d85320f0f0f69a75ff4de9432da80
gglymph <gglymph@gmail.com>
2020-06-16 10:16:54 -0400

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HelloComponent } from './hello.component';
 import { TextboxComponent } from './controls/textbox/textbox.component';
 import { RadioButtonsComponent } from './controls/radio-buttons/radio-buttons.component';
 import { SelectComponent } from './controls/select/select.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent, TextboxComponent, RadioButtonsComponent, SelectComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/controls/select/select.component.ts
+++ src/app/controls/select/select.component.ts
@@ -22,7 +22,11 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SelectComponent implements OnInit {
 
-  @Input() items: any;
+  @Input() items = [{value: 1, text: 'Option 1'},
+                    {value: 2, text: 'Option 2'},
+                    {value: 3, text: 'Option 3'},
+                    {value: 4, text: 'Option 4'}];
+                    
   @Input() formGroup: FormGroup;
   @Input() controlName: any;
   showList = false;
